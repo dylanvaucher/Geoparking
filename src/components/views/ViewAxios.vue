43159fc71dd027c4a30f6ea1047e22a5da027eb1
Dylan Vaucher <dylan.vaucher@heig-vd.ch>
2020-12-15 08:36:14 +0100

    Add geolocation, request json and geoadmin api search, wmts swissimage

--- src/components/views/ViewAxios.vue
+++ src/components/views/ViewAxios.vue
@@ -1,18 +1,24 @@
 <template>
+<div id="app-6">
+  <p>{{ lieu.lieu }}</p>
+  <p>{{ lieu.lat }}</p>
+  <p>{{ lieu.long }}</p>
+  <input v-model="searchquery" v-on:change="getGeoAdminSearch(searchapiURL,searchquery,searchtype)" placeholder="Recherchez de lieu">
+</div>
   <!-- Bulma: menu tabs -->
-  <div id="trump-card" class="card">
+  <div id="SITG-card" class="card">
     <div class="card-content">
       <p class="title">
-        “{{trumpquote}}”
+        “{{nbPlace}}”
       </p>
       <p class="subtitle">
-        Tronald Dump
+        SITG
       </p>
     </div>
     <footer class="card-footer">
       <p class="card-footer-item">
         <span>
-          Check more on <a target="_blank" rel="noopener noreferrer" :href="tronalddumpURL">@Tronald Dump</a>
+          Check more on <a target="_blank" rel="noopener noreferrer" :href="SITGURL">SITG Parking en temps réel</a>
         </span>
       </p>
     </footer>
@@ -22,14 +28,23 @@
 
 <script>
 import axios from 'axios'
+//import Vue from 'vue'
 
 export default {
   data(){
     return{
-      tronalddumpURL:"https://www.tronalddump.io/",
-      apiURL:"https://api.tronalddump.io/search/quote?query=",
-      query:"apologize",
-      trumpquote:""
+      SITGURL:"https://ge.ch/terags/rest/services/SITG/INFOMOB_PARKING_TEMPS_REEL/MapServer/0",
+      SITGapiURL:"https://ge.ch/terags/rest/services/SITG/INFOMOB_PARKING_TEMPS_REEL/MapServer/0/query?",
+      SITGquery:"where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson",
+      nbPlace:"",
+      searchapiURL: "https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText=",
+      searchquery:"",
+      searchtype: "&type=locations",
+      lieu: {
+        lieu: "",
+        lat: "",
+        long: ""
+      }
     }
   },
   methods: {
@@ -41,7 +56,7 @@ export default {
      * @returns {Promise<String>} quote from the API || default string if quote not found || default string if error
      * @catch error from the request 
      */
-    async getTrumptweet(url,query){
+    async getSITGParking(url,query){
       let response = await axios.get(url+query)
       .catch(error => {
         console.error(error);
@@ -56,17 +71,42 @@ export default {
         return "Didn't found any answers"
       }
       // Get first quote
-      return response.data._embedded.quotes[0].value;
+      console.log(response);
+      return response.data.features[11].attributes.PLACE_DISPONIBLE;
+    },
+
+    async getGeoAdminSearch(url,query,type){
+      let response = await axios.get(url+query+type)
+      .catch(error => {
+        console.error(error);
+        return 0
+      })
+      //request error
+      if(response == 0){
+        return "Ouch an error occurred"
+      }
+      // don't find an answer
+      if(response.data.count == 0){
+        return "Didn't found any answers"
+      }
+      // Get first quote
+      console.log(response);
+      this.lieu.lieu = response.data.results[0].attrs.label;
+      this.lieu.lat = response.data.results[0].attrs.lat;
+      this.lieu.long = response.data.results[0].attrs.lon;
+      //return response.data.results[0].attrs.label;
     }
   },
   async mounted() {
-    this.trumpquote = await this.getTrumptweet(this.apiURL,this.query)
+    this.searchquery;
+    this.nbPlace = await this.getSITGParking(this.SITGapiURL,this.SITGquery);
+    //this.lieu = await this.getGeoAdminSearch(this.searchapiURL,this.searchquery,this.searchtype)
   },
 }
 </script>
 
 <style scoped>
-#trump-card{
+#SITG-card{
   width: 50%;
   margin: auto;
 }
--- src/components/views/ViewOpenlayers.vue
+++ src/components/views/ViewOpenlayers.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="ol-container" class="map"></div>
+  <div id="mousePosition"></div>
 </template>
 
 <script>
@@ -7,15 +8,42 @@ import 'ol/ol.css';
 import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
+import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import WMTS  from 'ol/source/WMTS';
 import View from 'ol/View';
+import * as control from 'ol/control';
+import * as coordinate from 'ol/coordinate';
+import Projection from 'ol/proj/Projection';
 import * as olProj from 'ol/proj';
+import proj4 from 'proj4';
+import {register} from 'ol/proj/proj4';
+import {geoloc} from '../../lib/geolocation.js';
 
 export default {
   data() {
     return{
       center: [6.659361,46.779389],
       olmap:null,
-      zoom: 17
+      zoom: 17,
+      RESOLUTIONS: [
+              4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250,
+              1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5
+            ],
+      extent: [2420000, 130000, 2900000, 1350000],
+      matrixIds: [],
+      layerConfig: {
+        "attribution": "swisstopo",
+        "format": "jpeg",
+        "serverLayerName": "ch.swisstopo.swissimage",
+        "attributionUrl": "https://www.swisstopo.admin.ch/internet/swisstopo/fr/home.html",
+        "label": "SWISSIMAGE",
+        "timestamps": ["current"]
+      },
+      geooptions: {
+        enableHignAccuracy: true,
+        maximumAge        : 30000, 
+        timeout           : 27000
+      }
     }
   },
   computed:{
@@ -25,8 +53,22 @@ export default {
      * @use center in EPSG:4326
      * @return center in EPSG:3857
      */
+    projectionget(){
+      proj4.defs("EPSG:2056",
+        "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
+      );
+      register(proj4);
+      let projection = new Projection({
+        code: 'EPSG:2056',
+        extent: this.extent
+      })
+      return projection
+    },
     center3857(){
-      return olProj.transform(this.center, 'EPSG:4326', 'EPSG:3857');
+      return olProj.transform(this.center, 'EPSG:4326', 'EPSG:2056');
+    },
+    matrix(){
+      for(let i = 0; i < this.RESOLUTIONS.length; i++) {this.matrixIds.push(i);}
     }
   },
   methods: {
@@ -49,10 +91,82 @@ export default {
           zoom: mapzoom
         })
       })
+    },
+
+    wmtsSource(layerConfig, projection){
+      console.log(this.matrixIds);
+      let resolutions = layerConfig.resolutions || this.RESOLUTIONS;
+      let tileGrid = new WMTSTileGrid({
+        origin: [this.extent[0], this.extent[3]],
+        resolutions: resolutions,
+        matrixIds: this.matrixIds
+      });
+      let extension = layerConfig.format || 'png';
+      let timestamp = layerConfig['timestamps'][0];
+      return new WMTS(({
+        attributions: [new control.Attribution({
+          html: '<a target="new" href="https://www.swisstopo.admin.ch/' +
+            'internet/swisstopo/en/home.html">swisstopo</a>'
+        })],
+        url: '//wmts10.geo.admin.ch/1.0.0/{Layer}/default/' + timestamp + '/2056/{TileMatrix}/{TileCol}/{TileRow}.'+ extension,
+        tileGrid: tileGrid,
+        projection: projection,
+        layer: layerConfig.serverLayerName,
+        requestEncoding: 'REST'
+      }))
+    },
+
+    setupWMTSLayer(wmtssource){
+      return new TileLayer({
+        source: wmtssource
+      })
+    },
+
+    setupMap(layer, mapcenter, mapzoom, projection){
+      return new Map({
+        layers: [layer],
+        target: 'ol-container',
+        view: new View({
+          center: mapcenter,
+          zoom: mapzoom,
+          projection: projection,
+          resolution: 100
+        }),
+        controls: control.defaults({
+          attributionOptions:({
+            collapsible: false
+          })
+        }).extend([
+          new control.ScaleLine({
+            units: 'metric'
+          }),
+          new control.MousePosition({
+            coordinateFormat: coordinate.createStringXY(4),
+            //projection: 'EPSG:2056',
+            target: document.getElementById('mousePosition'),
+            undefinedHTML: '&nbsp;'
+          })
+        ]),
+        logo: false,
+      })
+    },
+
+    marker(position){
+      console.log(position.timestamp, position.coords.latitude, position.coords.longitude);
+      let coord = olProj.transform([position.coords.longitude,position.coords.latitude], 'EPSG:4326', 'EPSG:2056');
+      console.log(coord);
+    },
+
+    message(mess){
+      alert (mess);
     }
   },
+
   mounted() {
-    this.olmap = this.setupOpenlayersMap(this.center3857,this.zoom);
+    //this.olmap = this.setupOpenlayersMap(this.center3857,this.zoom);
+    this.matrix;
+    this.olmap = this.setupMap(this.setupWMTSLayer(this.wmtsSource(this.layerConfig, this.projectionget)),this.center3857, this.zoom, this.projectionget);
+    this.geoloc(this.marker, this.message, this.geooptions);
   }
 
 }
@@ -61,5 +175,10 @@ export default {
 <style scoped>
 #ol-container {
   height: 500px;
+}
+#mousePosition {
+  position: relative;
+  height: 20px;
+  width: 100%;
 }
 </style>
