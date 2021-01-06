796652a6b1618001924de78df7dd18b174dc45fd
Dylan Vaucher <dylan.vaucher@heig-vd.ch>
2021-01-06 07:41:46 +0100

    Modif fonction geoloc

--- src/lib/geolocation.js
+++ src/lib/geolocation.js
@@ -1,7 +1,7 @@
 export function geoloc(callback, errorcallback, geooptions){
     //
     if ("geolocation" in navigator) {
-        navigator.geolocation.watchPosition(callback,errorcallback, geooptions);
+        navigator.geolocation.watchPosition(callback, errorcallback, geooptions);
       } else {
         errorcallback('pas de geolocalisation')
         /* la géolocalisation n'est pas disponible */
