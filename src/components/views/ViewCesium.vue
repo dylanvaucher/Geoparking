aa59a4a0b9d6dd0dc4b478fc680e420386d7b2ec
Dylan Vaucher <dylan.vaucher@heig-vd.ch>
2021-01-06 07:45:25 +0100

    Import geoloc dans Cesium

--- src/components/views/ViewCesium.vue
+++ src/components/views/ViewCesium.vue
@@ -5,6 +5,7 @@
 <script>
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import * as Cesium from 'cesium';
+import {geoloc} from '../../lib/geolocation.js';
 
 export default {
   name: "CesiumGlobeView",
@@ -12,7 +13,12 @@ export default {
     return{
       center: [6.659361,46.779389],
       defaultheight:1500.,
-      viewer:null
+      viewer:null,
+      geooptions: {
+        enableHignAccuracy: true,
+        maximumAge        : 30000, 
+        timeout           : 27000
+      }
     }
   },
   methods: {
@@ -37,8 +43,30 @@ export default {
       let viewer = new Cesium.Viewer('cesium-container', {
         terrainProvider: new Cesium.createWorldTerrain()
       });
-      viewer.scene.primitives.add(Cesium.createOsmBuildings());
+      //viewer.scene.primitives.add(Cesium.createOsmBuildings());
       return viewer;
+    },
+
+    marker(position){
+      console.log(position.timestamp, position.coords.latitude, position.coords.longitude, position.coords.accuracy);
+      //let coord = [position.coords.longitude,position.coords.latitude];
+      this.posPoint(position.coords.longitude,position.coords.latitude);
+    },
+
+    message(mess){
+      alert (mess);
+    },
+
+    //dessin d'un cercle à la position
+    posPoint(long, lat){
+      this.viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(long, lat),
+        Point:{
+          pixelSize: 10,
+          color: Cesium.Color.YELLOW,
+        },
+      });
+      console.log('coucou')
     }
   },
   mounted() {
@@ -46,7 +74,10 @@ export default {
     Cesium.Ion.defaultAccessToken = process.env.VUE_APP_CESIUM_ION_TOKEN;
     
     this.viewer = this.setupCesiumGlobe();
-    this.flytodirection(this.center,this.defaultheight,this.viewer)
+    this.flytodirection(this.center,this.defaultheight,this.viewer);
+    // geoloc(this.marker, this.message, this.geooptions);
+    this.posPoint(this.center[0], this.center[1]);
+    this.viewer = this.posPoint(-75.59777, 40.03883);
   },
 };
 </script>
