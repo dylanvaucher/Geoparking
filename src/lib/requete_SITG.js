9b52870d9651d3467660ee67bb6ed55942678ede
Dylan Vaucher <dylan.vaucher@heig-vd.ch>
2021-01-07 15:13:06 +0100

    modification requete

--- src/lib/requete_SITG.js
+++ src/lib/requete_SITG.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
-export async function getSITGParking(url,query){
+export async function getSITGParking(){
+    let url = "https://ge.ch/terags/rest/services/SITG/INFOMOB_PARKING_TEMPS_REEL/MapServer/0/query?";
+    let query ="where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson";
     let response = await axios.get(url+query)
       .catch(error => {
         console.error(error);
         return 0
-      })
+      });
     //request error
     if(response == 0){
       return "Ouch an error occurred"
